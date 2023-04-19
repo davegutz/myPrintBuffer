864b19d93f4add95ecdbb518e9345d8b93efa85c
Dave G <davegutz@users.noreply.github.com>
2023-04-19 13:46:28 -0400

    buffering solution

--- countdown.py
+++ countdown.py
@@ -1,6 +1,11 @@
 from time import sleep
 
 for i in range(3, 0, -1):
-    print(i)
+    print(i)  # run in terminal with and without -u
+    # print(f"{i}\n", end="")
+    # print(i, "s", sep="\n", end="")
+    # print(i, end=" ")  # run in terminal
+    # print(i, end=" ", flush=True)  # run in terminal
+    # print(i, flush=True)  # run in terminal
     sleep(1)
 print("Go!")
